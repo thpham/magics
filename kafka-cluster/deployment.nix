f43900a4113a2134f4886c42df1ce39c63061052
Thomas Kim Pham <thomas.pham@ithings.ch>
2018-05-18 21:11:04 +0200

    make kafka/zk machines configurable through json file for multi cloud provider

--- kafka-cluster/deployment.nix
+++ kafka-cluster/deployment.nix
@@ -1,73 +1,63 @@
-let
-    
-    zookeeperInstanceSettings = [
-      { id = 0; name = "zk-0"; } 
-      { id = 1; name = "zk-1"; }
-      { id = 2; name = "zk-2"; }
-    ];
+{ machinesConfig ? builtins.readFile ./machines.json }:
 
-    kafkaInstanceSettings = [
-      { id = 0; name = "kafka-0"; }
-      { id = 1; name = "kafka-1"; }
-      { id = 2; name = "kafka-2"; }
-    ];
+let 
+  machines = builtins.fromJSON machinesConfig;
 
-    makeKafkaServer = machine: {   
-      name  = machine.name;
-      value =
-        { config, pkgs, lib, ... }:
-        {
-          networking.firewall.enable = false;
+  makeKafkaServer = machine: {   
+    name  = machine.name;
+    value =
+      { config, pkgs, lib, ... }:
+      {
+        networking.firewall.enable = false;
 
-          services.apache-kafka = {
-            enable = true;
-            logDirs = [
-              "/data"
-            ];
-            brokerId  = machine.id;
-            hostname  = "${config.networking.privateIPv4}";
-            zookeeper = lib.concatStringsSep "," (map (x: x.name) zookeeperServers);
-            extraProperties = "zookeeper.connection.timeout.ms=600000";
-            jvmOptions = [
-              "-server"
-              "-Xmx2048M"
-              "-Xms1024M"
-              "-XX:+UseCompressedOops"
-              "-XX:+UseParNewGC"
-              "-XX:+UseConcMarkSweepGC"
-              "-XX:+CMSClassUnloadingEnabled"
-              "-XX:+CMSScavengeBeforeRemark"
-              "-XX:+DisableExplicitGC"
-              "-Djava.awt.headless=true"
-              "-Djava.net.preferIPv4Stack=true"
-            ];
-          };
+        services.apache-kafka = {
+          enable = true;
+          logDirs = [
+            "/data"
+          ];
+          brokerId  = machine.id;
+          hostname  = "${config.networking.privateIPv4}";
+          zookeeper = lib.concatStringsSep "," (map (x: x.name) zookeeperServers);
+          extraProperties = "zookeeper.connection.timeout.ms=600000";
+          jvmOptions = [
+            "-server"
+            "-Xmx2048M"
+            "-Xms1024M"
+            "-XX:+UseCompressedOops"
+            "-XX:+UseParNewGC"
+            "-XX:+UseConcMarkSweepGC"
+            "-XX:+CMSClassUnloadingEnabled"
+            "-XX:+CMSScavengeBeforeRemark"
+            "-XX:+DisableExplicitGC"
+            "-Djava.awt.headless=true"
+            "-Djava.net.preferIPv4Stack=true"
+          ];
         };
-    };
+      };
+  };
+  kafkaServers = map makeKafkaServer machines.kafka.configs;
+  
+  makeZookeeperServer = machine: {
+    name  = machine.name;
+    value =
+      { config, pkgs, lib, ... }:
+      {          
+        networking.firewall.enable = false;
 
-    makeZookeeperServer = machine: {
-      name  = machine.name;
-      value =
-        { config, pkgs, lib, ... }:
-        {          
-          networking.firewall.enable = false;
-
-          services.zookeeper = {
-            enable  = true;
-            dataDir = "/data";
-            id = machine.id;
-            servers =
-              let toLine = n: x: "server.${toString (builtins.sub n 1)}=${x.name}:2888:3888\n";
-              in lib.concatImapStrings toLine zookeeperServers;
-          };
+        services.zookeeper = {
+          enable  = true;
+          dataDir = "/data";
+          id = machine.id;
+          servers =
+            let toLine = n: x: "server.${toString (builtins.sub n 1)}=${x.name}:2888:3888\n";
+            in lib.concatImapStrings toLine zookeeperServers;
         };
-    };
-
-    zookeeperServers = map makeZookeeperServer zookeeperInstanceSettings;
-    kafkaServers     = map makeKafkaServer kafkaInstanceSettings;
+      };
+  };
+  zookeeperServers = map makeZookeeperServer machines.zookeeper.configs;
 
 in  { 
-  network.description = "kafka-cluster";
+  network.description = "kafkazk-cluster";
   network.enableRollback = true;
 
   defaults = {
--- kafka-cluster/ec2.nix
+++ kafka-cluster/ec2.nix
@@ -1,68 +1,70 @@
 { aws-profile ? "default",
   environment ? "dev",
-  aws-region  ? "us-east-1" }:
+  aws-region  ? "us-east-1",
+  machinesConfig ? builtins.readFile ./machines.json }:
 
 let
   region = aws-region;
   accessKeyId = aws-profile;
 
-  deployment = ec2Deployment: filesys:
+  machines = builtins.fromJSON machinesConfig;
+
+  makeEC2configs = ec2Deployment: filesys:
     { resources, ... }:
     { 
       deployment.targetEnv = "ec2";
       deployment.ec2 = {
-          keyPair = resources.ec2KeyPairs.k8s-key-pair;
+          keyPair = resources.ec2KeyPairs.kafkazk-key-pair;
           accessKeyId = accessKeyId;
           region = region;
-          subnetId = "subnet-";   # to be adapted accordingly, e.g. default
           ebsInitialRootDiskSize = 20;
           usePrivateIpAddress = false;  # should be true if managed within vpc
-          associatePublicIpAddress = false;
+          associatePublicIpAddress = true;  # should be false if hidden within vpc
         } // ec2Deployment;
       fileSystems = filesys;
     };
-
-  zookeeper = deployment {
-    instanceType = "m4.large";
-    associatePublicIpAddress = true;
-    #securityGroupIds = [ "sg-" ];
-    tags = {
-      Environment = environment;
-      Group = "zookeeper";
-    }; 
-  } {};
-
-  kafka = deployment {
-    instanceType = "m4.large";
-    associatePublicIpAddress = true; # should be false if hidden in vpc
-    #securityGroupIds = [ "sg-" ];
-    tags = {
-      Environment = environment;
-      Group = "kafka";
-    }; 
-  } {
-    "/data" = {
-      autoFormat = true;
-      fsType = "ext4";
-      device = "/dev/xvdf";
-      ec2.size = 30;
-      ec2.volumeType = "gp2";
-      ec2.deleteOnTermination = true; # should be false by security
-    };
+  
+  makeZookeeperServer = machine: {
+    name  = machine.name;
+    value = makeEC2configs {
+        instanceType = machines.zookeeper.aws.instanceType;
+        securityGroupIds =  machines.zookeeper.aws.securityGroupIds;
+        subnetId = machine.aws-subnetId;
+        tags = {
+          Environment = environment;
+          Group = "zookeeper";
+        }; 
+      } {};
   };
+  zookeeperServers = map makeZookeeperServer machines.zookeeper.configs;
+
+  makeKafkaServer = machine: {
+    name  = machine.name;
+    value = makeEC2configs {
+        instanceType = machines.kafka.aws.instanceType;
+        securityGroupIds = machines.kafka.aws.securityGroupIds;
+        subnetId = machine.aws-subnetId;
+        tags = {
+          Environment = environment;
+          Group = "kafka";
+        }; 
+      } {
+        "/data" = {
+          autoFormat = true;
+          fsType = "ext4";
+          device = "/dev/xvdf";
+          ec2.size = machines.kafka.aws.storageSize;
+          ec2.volumeType = "gp2";
+          ec2.deleteOnTermination = true; # should be false for security
+        };
+      };
+  };
+  kafkaServers = map makeKafkaServer machines.kafka.configs;
 
 in {
-
-  zk-0 = zookeeper;
-  zk-1 = zookeeper;
-  zk-2 = zookeeper;
-  
-  kafka-0 = kafka;
-  kafka-1 = kafka;
-  kafka-2 = kafka;
-
   # Provision an EC2 key pair.
-  resources.ec2KeyPairs.k8s-key-pair =
+  resources.ec2KeyPairs.kafkazk-key-pair =
     { inherit region accessKeyId; };
-
 }
+//  builtins.listToAttrs zookeeperServers
+//  builtins.listToAttrs kafkaServers
