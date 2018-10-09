c418fd9065f3e2fb51a737b4a6e879cba2190f15
Thomas Kim Pham <thomas.pham@ithings.ch>
2018-10-10 00:01:21 +0200

    add custom grafana package

---
+++ pkgs/grafana/default.nix
@@ -0,0 +1,34 @@
+{ pkgs }:
+
+pkgs.buildGoPackage rec {
+  version = "5.2.4";
+  name = "grafana-${version}";
+  goPackagePath = "github.com/grafana/grafana";
+
+  src = pkgs.fetchFromGitHub {
+    rev = "v${version}";
+    owner = "grafana";
+    repo = "grafana";
+    sha256 = "15w935i22ddx6ff32ynypjh3q670vnrj74qw0vdkxdmrlwk3q7wc";
+  };
+
+  srcStatic = pkgs.fetchurl {
+    url = "https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-${version}.linux-amd64.tar.gz";
+    sha256 = "187dqjahz1z1gkcx9pxnf2hri6g3b5j3ppadwfahz0rwsqj4v2lf";
+  };
+
+  postPatch = ''
+    substituteInPlace pkg/cmd/grafana-server/main.go \
+      --replace 'var version = "5.0.0"'  'var version = "${version}"'
+  '';
+
+  preBuild = "export GOPATH=$GOPATH:$NIX_BUILD_TOP/go/src/${goPackagePath}/Godeps/_workspace";
+  
+  postInstall = ''
+    tar -xvf $srcStatic
+    mkdir -p $bin/share/grafana
+    mv grafana-*/{public,conf,tools} $bin/share/grafana/
+    ln -sf ${pkgs.phantomjs2}/bin/phantomjs $bin/share/grafana/tools/phantomjs/phantomjs
+  '';
+
+}
