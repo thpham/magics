5647fec3ba7a458c438fa9509165abb52e9952b3
Thomas Kim Pham <thomas.pham@ithings.ch>
2018-10-16 14:16:40 +0200

    add influxdb http authentication for remote_write/read + prometheus job for local services monitoring

--- monitor/deployment.nix
+++ monitor/deployment.nix
@@ -3,19 +3,40 @@
 let 
   machines = builtins.fromJSON (builtins.readFile machinesConfigPath);
 
+  influxDBAdminPass = "neeW8fu4Rae7";
+
   makeMonitorServer = machine: {   
     name  = machine.name;
     value =
       { config, pkgs, lib, ... }:
-      {
-        imports = [ ../modules/services/prometheus/default.nix  ];
-        disabledModules = [ "services/monitoring/prometheus/default.nix" ];
+      let
+        #configs = import ./configs.nix { inherit pkgs; };
+
+        influxdbInit = pkgs.writeText "influxdb-init.txt" ''
+          # DDL
+          CREATE DATABASE "prometheus"
+          CREATE USER ${machine.influxdbUser} WITH PASSWORD '${machine.influxdbPass}'
+          GRANT ALL ON "prometheus" TO ${machine.influxdbUser}
+          ALTER RETENTION POLICY "autogen" ON "prometheus" DURATION 1d REPLICATION 1 SHARD DURATION 1d DEFAULT
+        '';
+
+      in {
+
+        imports = [
+          ../modules/services/prometheus/default.nix
+          ../modules/services/databases/influxdb.nix
+        ];
+        disabledModules = [
+          "services/monitoring/prometheus/default.nix"
+          "services/databases/influxdb.nix"
+        ];
         
         services = {
           influxdb = {
             enable = true;
             package = pkgs.influxdb;
             dataDir = "/data/influxdb";
+            #extraConfig = '' '';
           };
           prometheus = {
             enable = true;
@@ -25,31 +46,26 @@ let
             ];
             remoteWriteConfigs = [
               {
-                url = "http://localhost:8086/api/v1/prom/write?db=prometheus";
+                url = "http://localhost:8086/api/v1/prom/write?u=${machine.influxdbUser}&p=${machine.influxdbPass}&db=prometheus";
               }
             ];
             remoteReadConfigs = [
               {
-                url = "http://localhost:8086/api/v1/prom/read?db=prometheus";
+                url = "http://localhost:8086/api/v1/prom/read?u=${machine.influxdbUser}&p=${machine.influxdbPass}&db=prometheus";
               }
             ];
             scrapeConfigs = [
               {
-                job_name = "prometheus";
+                job_name = "${machine.name}";
                 scrape_interval = "1m";
                 static_configs = [
-                  {
-                    targets = [
-                      "localhost:9090"
-                    ];
-                    labels = {
-                      alias = "monitor";
-                    };
-                  }
+                  {targets = ["localhost:9090"]; labels = { alias = "prometheus"; };}
+                  {targets = ["localhost:8086"]; labels = { alias = "influxdb"; };}
+                  {targets = ["localhost:3000"]; labels = { alias = "grafana"; };}
                 ];
               }
             ];
-            #configText = builtins.readFile machine.prometheusConfigFile;
+            #configText = configs.prometheusConfig;
             exporters.node = {
               enable = true;
               listenAddress = "localhost";
@@ -72,10 +88,28 @@ let
         };
         networking.firewall = {
           allowedTCPPorts = [
+            80 443 # HTTP/S
             9090   # Prometheus
             8086   # InfluxDB HTTP service
             3000   # Grafana
           ];
+        };
+
+        systemd = {
+          services.influxdb-init = {
+            wantedBy = [ "influxdb.service" ];
+            after = [ "influxdb.service" ];
+            serviceConfig = {
+              Type = "oneshot";
+              RemainAfterExit = true ;
+              ExecStart = with pkgs; writeScript "influxdb-init.sh" ''
+                #! ${bash}/bin/sh
+                ${coreutils}/bin/sleep 2
+                ${influxdb}/bin/influx -execute "CREATE USER admin WITH PASSWORD '${influxDBAdminPass}' WITH ALL PRIVILEGES"
+                ${influxdb}/bin/influx -username 'admin' -password '${influxDBAdminPass}' -import -path=${influxdbInit}
+              '';
+            };
+          };
         };
 
         environment.systemPackages = with pkgs; [
