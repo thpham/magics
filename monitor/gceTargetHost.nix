a4be4c136fe72310acd9da8a93772e099275a5a2
Thomas Kim Pham <thomas.pham@ithings.ch>
2018-10-29 20:45:41 +0100

    enable data disk in gce

--- monitor/gceTargetHost.nix
+++ monitor/gceTargetHost.nix
@@ -16,10 +16,10 @@ let
         # Be careful, not to have a /etc/boto.cfg set (i.e. http://bit.ly/2MNBsL7)
         imports = [<nixpkgs/nixos/modules/virtualisation/google-compute-config.nix>];
 
-        #fileSystems."/data" = {
-        #  device = "/dev/disk/by-label/data";
-        #  autoResize = true;
-        #};
+        fileSystems."/data" = {
+          device = "/dev/sdb";
+          autoResize = true;
+        };
 
       };
   };
